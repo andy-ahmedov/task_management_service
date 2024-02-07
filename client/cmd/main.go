c83fd52961a9eaea96ed23798c6dd2f75d6a7580
Andy <andy.ahmedov2@gmail.com>
2024-02-07 15:41:25 +0300

    Write the main server function
    A main function has been written that uses common
    config and logger packages. A grpc client has been
    implemented that sends data via the rpc protocol.
    
    [Ticket: 6]

---
+++ client/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	grpc_client "github.com/andy-ahmedov/task_management_service/client/internal/transport"
+	"github.com/andy-ahmedov/task_management_service/service_api/api"
+	"github.com/andy-ahmedov/task_management_service/service_api/config"
+	"github.com/andy-ahmedov/task_management_service/service_api/logger"
+)
+
+func main() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logg := logger.NewLogger()
+	logg.Info(cfg)
+
+	createClient, err := grpc_client.NewClient(cfg.Srvr.Port)
+	if err != nil {
+		logg.Fatal(err)
+	}
+
+	err = createClient.Create(context.Background(), &api.CreateRequest{
+		Name:        "HEYYO GRPC",
+		Description: "BELIEVE?",
+		Status:      "I DONT KNOW",
+	})
+	if err != nil {
+		logg.Fatal(err)
+	}
+}
