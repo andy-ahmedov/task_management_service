0f98cad4de2f91b40c0fc58810d00465799fc029
Andy <andy.ahmedov2@gmail.com>
2024-02-08 21:19:30 +0300

    Rewrite the client for use via the terminal
    The library github.com/spf13/cobra was used
    to create an interface for working with the
    client via the command line. The implementation
    of the functionality is divided into files,
    according to the logic being executed.
    
    [Ticket: 17]

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
 
-	"github.com/andy-ahmedov/task_management_service/client/internal/domain"
+	"github.com/andy-ahmedov/task_management_service/client/internal/commands"
 	grpc_client "github.com/andy-ahmedov/task_management_service/client/internal/transport/grpc"
 	"github.com/andy-ahmedov/task_management_service/service_api/config"
 	"github.com/andy-ahmedov/task_management_service/service_api/logger"
+	"github.com/spf13/cobra"
 )
 
 func main() {
@@ -24,36 +24,13 @@ func main() {
 		logg.Fatal(err)
 	}
 
-	// err = client.Create(context.Background(), "NEW Task", "HRA", "ON")
-	// if err != nil {
-	// 	logg.Fatal(err)
-	// }
+	var rootCmd = &cobra.Command{Use: "app"}
+	cmdCreate := commands.CommandCreate(client, rootCmd)
+	cmdUpdate := commands.CommandUpdate(client, rootCmd)
+	cmdDelete := commands.CommandDelete(client, rootCmd)
+	cmdGetAll := commands.CommandGetAll(client, rootCmd)
+	cmdGet := commands.CommandGet(client, rootCmd)
 
-	// task, err := client.Get(context.Background(), 2)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println(task)
-
-	// tasks, err := client.GetAll(context.Background())
-	// fmt.Println(tasks)
-
-	// err = client.Delete(context.Background(), 2)
-	// if err != nil {
-	// 	logg.Fatal(err)
-	// }
-
-	that := "FAST"
-
-	upd := domain.UpdateTaskInput{
-		Name:        &that,
-		Description: nil,
-		Status:      &that,
-	}
-
-	err = client.Update(context.Background(), 5, &upd)
-	if err != nil {
-		logg.Fatal(err)
-	}
+	rootCmd.AddCommand(cmdCreate, cmdUpdate, cmdGet, cmdGetAll, cmdDelete)
+	rootCmd.Execute()
 }
